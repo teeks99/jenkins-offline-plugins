da144fc6c8f7ce70056a01cba4eb804fea0de75d
Thomas Kent <tom@teeks99.com>
2019-08-12 16:45:30 -0500

    Added better argument parsing

--- download_plugins.py
+++ download_plugins.py
@@ -1,5 +1,10 @@
+__author__ = "Thomas Kent"
+__copyright__ = "Copyright (C) 2019 Thomas Kent"
+__license__ = "Apache 2.0"
+
 import json
 import urllib.request
+import urllib.parse
 import os
 import argparse
 
@@ -9,10 +14,11 @@ official_plugin_location = "https://updates.jenkins-ci.org/download/plugins/"
 class JenkinsDownloader(object):
     def __init__(self, update_list=official_update_list,
                  plugin_location=official_plugin_location,
-                 plugins=[]):
+                 plugins=[], headers={}):
         self.update_list = update_list
         self.plugin_location = plugin_location
         self.plugins = plugins
+        self.headers = headers
 
         self._update_data_cache = None
 
@@ -25,8 +31,11 @@ class JenkinsDownloader(object):
         self.download_all(download_location)
 
     def add_plugin(self, plugin):
+        if not plugin in self.plugin_dict:
+            print("Invalid plugin lookup: " + plugin)
+            return
         data = self.plugin_dict[plugin]
-        self.to_download[plugin] = data["url"]
+        self.to_download[plugin] = self.url(data)
         if "dependencies" in data:
             for dep in data["dependencies"]:
                 self.add_plugin(dep["name"])
@@ -36,13 +45,17 @@ class JenkinsDownloader(object):
         if self._update_data_cache:
             return self._update_data_cache["plugins"]
 
-        with urllib.request.urlopen(self.update_list) as f:
+        r = urllib.request.Request(self.update_list, headers=self.headers)
+        with urllib.request.urlopen(r) as f:
             lines = f.readlines()
             assert lines[0] == b"updateCenter.post(\n"
             assert lines[2] == b");"
             self._update_data_cache = json.loads(lines[1].strip())
         
         return self._update_data_cache["plugins"]
+
+    def url(self, data):
+        return data["url"]
 
     def download_all(self, download_location):
         if not os.path.isdir(download_location):
@@ -53,7 +66,8 @@ class JenkinsDownloader(object):
 
     def download_file(self, url, download_location):
         print("Getting: " + url)
-        with urllib.request.urlopen(url) as f:
+        r = urllib.request.Request(url, headers=self.headers)
+        with urllib.request.urlopen(r) as f:
             fname = os.path.basename(f.geturl())
             full_loc = os.path.join(download_location, fname)
             print(" to " + full_loc)
@@ -67,25 +81,63 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(
         description=description)
-    parser.add_argument('-u', '--update-list', default=official_update_list,
-                        help='update list to use')
-    parser.add_argument('-d', '--plugin-location',
-                        default=official_plugin_location,
-                        help='location to download plugins from')
-    parser.add_argument('-c', '--config', default=None,
-                        help='config file with options')
-    parser.add_argument('-p', '--plugin', store="append",
-                        help='plugin to search for')
+    parser.add_argument(
+        '-u', '--update-list', default=None,
+        help='update list to use (default={})'.format(official_update_list))
+    parser.add_argument(
+        '-l', '--plugin-location', default=None,
+        help='location to download plugins from (default={})'.format(
+        official_plugin_location))
+    parser.add_argument(
+        '-c', '--config', default=None, help='config file with options')
+    parser.add_argument(
+        '-p', '--plugin', action='append', dest='plugins',
+        help='plugin name (add this option as many times as needed')
+    parser.add_argument(
+        '-H', '--header', action='append', dest='headers',
+        help='colon seperated curl header, e.g. -H X-First-Name:Joe')
+    parser.add_argument(
+        '-d', '--download', default='download',
+        help="location to download plugins into (create if doesn't exist")
 
     args = parser.parse_args()
 
+    plugins = []
+    headers = {}
+    update_list = official_update_list
+    plugin_location = official_plugin_location
 
     if args.config:
         with open(args.config) as f:
             start_data = json.load(f)
 
+        if "update_list" in start_data:
+            update_list = start_data["update_list"]
+
+        if "plugin_location" in start_data:
+            plugin_location = start_data["plugin_location"]
+
+        if "plugins" in start_data:
+            plugins.extend(start_data["plugins"])
+
+        if "headers" in start_data:
+            headers.update(start_data["headers"])
+
+    if args.update_list:
+        update_list = args.update_list
+
+    if args.plugin_location:
+        plugin_location = args.plugin_location
+
+    if args.plugins:
+        plugins.extend(args.plugins)
+
+    if args.headers:
+        for hs in args.headers:
+            key, value = hs.split(':')
+            headers[key] = value
+
     j = JenkinsDownloader(
-        update_list=start_data["update_list"],
-        plugin_location=start_data["plugin_location"],
-        plugins=start_data["plugins"])
-    j.get("download")
+        update_list=update_list, plugin_location=plugin_location,
+        plugins=plugins, headers=headers)
+    j.get(args.download)
